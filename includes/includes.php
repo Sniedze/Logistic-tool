1540cf4cf55f81672663810b49e07932a8441842
Ulrika Sniedze <u.sniedze@gmail.com>
2019-11-27 20:56:15 +0100

    Creates connection file

---
+++ includes/includes.php
@@ -0,0 +1,43 @@
+<?php
+/
+    class DB {
+
+        /**
+         * Opens a connection to the database
+         * 
+         * @returns a connection object
+         */
+        public function connect() {
+            $server = 'localhost';
+            $db = 'logistic_tool';
+            $user = 'root';
+            $pwd = '';
+
+            $DSN = 'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8';
+            
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+
+            try {
+                $connections_to_db = new PDO($DSN, $user, $pwd, $options); 
+            } catch (PDOException $e) {
+                echo 'Connection unsuccessful';
+                die('Connection unsuccessful: ' . $connections_to_db->connect_error());
+                exit();
+            }
+            
+            return($connections_to_db);   
+        }
+
+        /**
+         * Closes a connection to the database
+         * 
+         * @param the connection object to disconnect
+         */
+        public function disconnect($conobj) {
+            $conobj = null;
+        }
+    }
