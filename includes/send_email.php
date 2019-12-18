4347d11280d9cb31f4a9857bfc457a3300317f92
RytisDr <r.drazdauskas@gmail.com>
2019-12-18 15:14:47 +0200

    Removes login info from mail script.

--- includes/send_email.php
+++ includes/send_email.php
@@ -21,10 +21,10 @@ function sendEmail($email, $type)
         //Server settings
         $mail->SMTPDebug = 0;                                       // Enable verbose debug output
         $mail->isSMTP();                                            // Set mailer to use SMTP
-        $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->Host       = '';  // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'php.auto.sender.email@gmail.com';                     // SMTP username
-        $mail->Password   = 'passout112';                               // SMTP password
+        $mail->Username   = '';                     // SMTP username
+        $mail->Password   = '';                               // SMTP password
         $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
 
